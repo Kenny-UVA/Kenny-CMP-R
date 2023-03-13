4d407145de7c3b5ae18d161e5879d6b1b45da89e
Kenny-UVA <canric.wout@student.uva.nl>
2023-03-13 11:55:18 +0100

    chordograms

--- Untitled.R
+++ Untitled.R
@@ -2,21 +2,42 @@ library(tidyverse)
 library(spotifyr)
 library(compmus)
 library(dplyr)
-library(tidytext)
+library(conflicted)
 
+bct <-
+  get_tidy_audio_analysis("4kXxEhuatrvwrTQycA7s9B") |> # Change URI.
+  compmus_align(bars, segments) |>                     # Change `bars`
+  select(bars) |>                                      #   in all three
+  unnest(bars) |>                                      #   of these lines.
+  mutate(
+    pitches =
+      map(segments,
+          compmus_summarise, pitches,
+          method = "rms", norm = "euclidean"              # Change summary & norm.
+      )
+  ) |>
+  mutate(
+    timbre =
+      map(segments,
+          compmus_summarise, timbre,
+          method = "rms", norm = "euclidean"              # Change summary & norm.
+      )
+  )
 
-bzt <- 
-  get_tidy_audio_analysis('5ZLkc5RY1NM4FtGWEd6HOE') %>% 
-  compmus_align(bars, segments) %>% 
-  select(bars) %>% unnest(bars) %>% 
-  mutate(
-    pitches = 
-      map(segments, 
-          compmus_summarise, pitches, 
-          method = 'rms', norm = 'euclidean')) %>% 
-  mutate(
-    timbre = 
-      map(segments, 
-          compmus_summarise, timbre, 
-          method = 'mean'))
+bct |>
+  compmus_self_similarity(timbre, "manhattan") |> 
+  ggplot(
+    aes(
+      x = xstart + xduration / 2,
+      width = xduration,
+      y = ystart + yduration / 2,
+      height = yduration,
+      fill = d
+    )
+  ) +
+  geom_tile() +
+  coord_fixed() +
+  scale_fill_viridis_c(guide = "none") +
+  theme_classic() +
+  labs(x = "", y = "")
 
